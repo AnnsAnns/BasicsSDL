1e14e6442b772c7677bd24b05906bd5e85e6bfa0
tomGER <tumger@outlook.de>
2020-09-20 15:51:00 +0200

    Implement Entities

--- src/renderwindow.cpp
+++ src/renderwindow.cpp
@@ -36,7 +36,19 @@ void RenderWindow::clear() {
 }
 
 void RenderWindow::render(SDL_Texture* p_tex) {
-    SDL_RenderCopy(renderer, p_tex, NULL, NULL);
+    SDL_Rect src;
+    src.x = 0;
+    src.y = 0;
+    src.w = 32;
+    src.h = 32;
+
+    SDL_Rect dst;
+    dst.x = 0;
+    dst.y = 0;
+    dst.w = 32;
+    dst.h = 32;
+
+    SDL_RenderCopy(renderer, p_tex, &src, &dst);
 }
 
 void RenderWindow::display() {
